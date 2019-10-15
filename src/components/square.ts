95a6425f7c64bfcd917f846c79b14ad7e725a977
methoil <mateev.br@gmail.com>
2019-10-15 00:22:33 -0400

    feat - add square component

---
+++ src/components/square.ts
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Square(props) {
+    return (
+        <button className={`square-${props.shade}`}
+            onClick={props.onClick}
+            style={props.style}>
+            </button>
+    );
+}
