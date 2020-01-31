09dd9ea8da295cd0060f416ab9acc69b3e20e931
methoil <mateev.br@gmail.com>
2020-01-31 16:03:31 -0500

    feat(lobby): add rooms component

---
+++ front-end/src/components/lobby/rooms.tsx
@@ -0,0 +1,51 @@
+// chess-ui/src/Rooms.js
+import React from 'react';
+import { List, Icon, Header } from 'semantic-ui-react';
+
+interface IRoom {
+  id: string;
+  name: string;
+}
+
+type IEnterRoom = (roomId: string) => {};
+type ILeaveRoom = (roomId: string) => {};
+
+export default function Rooms(props: {
+  joined: IRoom[];
+  joinable: IRoom[];
+  activeRoom: string;
+  enterRoom: IEnterRoom;
+  leaveRoom: ILeaveRoom;
+}) {
+  const joinedRooms = props.joined.map(room => (
+    <List.Item key={room.id}>
+      <List.Content floated="right">
+        <a onClick={() => props.leaveRoom(room.id)}>Leave</a>
+      </List.Content>
+      <Icon name={room.id === props.activeRoom ? 'angle double right' : undefined} />
+      <List.Content>
+        <a onClick={() => props.enterRoom(room.id)}>{room.name}</a>
+      </List.Content>
+    </List.Item>
+  ));
+  const joinableRooms = props.joinable.map(room => (
+    <List.Item key={room.id}>
+      <Icon name={undefined} />
+      <List.Content>
+        <a onClick={() => props.enterRoom(room.id)}>{room.name}</a>
+      </List.Content>
+    </List.Item>
+  ));
+  return (
+    <div>
+      <Header as='h4'>Active Rooms</Header>
+      <List divided relaxed>
+        {joinedRooms}
+      </List>
+      <Header as='h4'>Joinable Rooms</Header>
+      <List divided relaxed>
+        {joinableRooms}
+      </List>
+    </div>
+  );
+}
