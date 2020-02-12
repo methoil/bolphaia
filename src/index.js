898b72b0e3eb954b887700035d2c01c034f6d2eb
Boris Mateev <47762021+methoil@users.noreply.github.com>
2020-02-12 12:20:43 -0500

    fix(router): set basepath
    needed for router to work in deployed environment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 
 ReactDOM.render(
   // <Game />,
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}`>
     <App />
   </BrowserRouter>,
   document.getElementById('root'),
