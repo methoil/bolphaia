2e56be82274dad6d0657d20973079ac803bf0108
methoil <mateev.br@gmail.com>
2020-02-17 13:25:40 -0500

    feat - add lore for pieces

---
+++ src/components/pieces/pieceConfig.ts
@@ -0,0 +1,75 @@
+export const pieceDefs = {
+  levy: {
+    pieceType: 'levy',
+    imageLocation: "../../resources/levyBlack.svg",
+    health: 3,
+    attack: 1,
+    lore:
+      'Peasants and laborers are often conscripted into the army or forced to join out of sheer desparation. Armed with a small shield and spear, they are even more poorly trained than they are armed.',
+    gameplay:
+      'Levies are best used to protect more valuable soldiers by distracting enemies. They have, however, been known to suprise careless foes with their disorienting mob tactics.',
+  },
+  archer: {
+    pieceType: 'archer',
+    imageLocation: "../../resources/archerBlack.svg",
+    health: 2,
+    lore:
+      'Skilled archers are always in short supply, as good marksman require years of practice or experience from hunting since a young age. While a sword cannot strike from a distance like an arrow, these men are lightly armed and will fall quickly in melee combat.',
+    gameplay: '',
+  },
+  legion: {
+    pieceType: 'legion',
+    imageLocation: "../../resources/legionBlack.svg",
+    health: 8,
+    lore:
+      'Legions are professional, heavily armed infantry that form the backbone of our army. They are able to stand their ground as they march in a tight, solid formation, though as a result they move very slowly.',
+    gameplay: '',
+  },
+  cataphract: {
+    pieceType: 'cataphract',
+    imageLocation: "../../resources/ctaphractBlack.svg",
+    health: 8,
+    lore:
+      'Recent developments in horsemanship have given rise to a new class of warrior. Cataphracts are wealthy aristocrats who ride into battle very heavily armed on powerful horses. They are few mortals who can withstand their crushing charge.',
+    gameplay: '',
+  },
+  chariot: {
+    pieceType: 'chariot',
+    imageLocation: "../../resources/chariotBlack.svg",
+    health: 10,
+    lore:
+      'Horse drawn chariots provide well armed soldiers a solid platform to attack from with greatly enhanced mobility. While slower than mounted units, they can chase down infantry and have impressive durability due to their solid construction and disciplined crews.',
+  },
+  warElephant: {
+    pieceType: 'warElephant',
+    imageLocation: "../../resources/warElephantBlack.svg",
+    health: 20,
+    lore:
+      'These massive beasts, imported from the far east, are a terrifying sight to any opponent. Their strenght is unmatched and it will take many blows to bring one of them down.',
+    gameplay: '',
+  },
+  lightCavalry: {
+    pieceType: 'lightCavalry',
+    imageLocation: "../../resources/lightCavalryBlack.svg",
+    health: 4,
+    lore:
+      'Migrant barbarians and more reckless youths of the nobility not yet old enough to join elite units often join our ranks as irregular cavalry. What they lack in discipline and amor, they make up for with speed and daring.',
+    gameplay: '',
+  },
+  centaur: {
+    pieceType: 'centaur',
+    imageLocation: "../../resources/centaurBlack.svg",
+    health: 4,
+    lore:
+      'Centaurs are reclusive, forest folk who rarely venture into the realm of men; in fact, most ration people do not believe they exist. They do make outstanding marksman and for the right price, they can potentially be hired as mercenaries.',
+    gameplay: '',
+  },
+  general: {
+    pieceType: 'general',
+    imageLocation: "../../resources/generalBlack.svg",
+    health: 10,
+    lore:
+      "You have been given command of one of The Empire's grandest armies and are protected by a bodyguard of elite cataphracts. While a formidable warrior yourself, you are needed to issue orders, and if you fall, your army will fall into disarray and route.",
+    gameplay: '',
+  },
+};
