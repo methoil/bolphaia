59d67b97e677376e1b1d906450a81e0e21fad40a
Boris Mateev <mateev.br@gmail.com>
2020-04-27 23:25:49 -0400

    test - test for generateEmptyHighlightedMoves and add mock for BoardState

--- src/components/game/services/_tests/moveHighlightSvc.spec.ts
+++ src/components/game/services/_tests/moveHighlightSvc.spec.ts
@@ -1,4 +1,33 @@
-import { getValidIndex } from "../moveHighlightSvc";
+import {
+  getValidIndex,
+  generateEmptyHighlightedMoves,
+} from "../moveHighlightSvc";
+import { BOARD_WIDTH, BOARD_HEIGHT } from "../boardSetup";
+
+describe("generateEmptyHighlightedMoves", () => {
+  const testMatrix = generateEmptyHighlightedMoves();
+
+  test("should have BOARD_WIDTH number of columns", () => {
+    expect(testMatrix[0].length).toBe(BOARD_WIDTH);
+  });
+
+  test("should have BOARD_HEIGHT number of rows", () => {
+    expect(testMatrix.length).toBe(BOARD_HEIGHT);
+  });
+  test("should each entry in the matrix be an PossibleMove with everything set to false", () => {
+    const emptyMove = {
+      canMove: false,
+      canAttack: false,
+      inAttackRange: false,
+    };
+
+    for (let x = 0; x < BOARD_HEIGHT; x++) {
+      for (let y = 0; y < BOARD_WIDTH; y++) {
+        expect(testMatrix[x][y]).toEqual(emptyMove);
+      }
+    }
+  });
+});
 
 describe("getValidIndex", () => {
   test("should return the input index if it is within range", () => {
